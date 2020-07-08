fdc570229af0adbcbe8ab8d08ad064a6d0711e5d
Carlos Acuña <6326005-Acunnia@users.noreply.gitlab.com>
2020-07-08 23:15:00 +0200

    Task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: ['.white{color: white}']
 })
 export class AppComponent {
+  pressed = false;
+  count = [];
+
+  clickHandle() {
+    this.pressed = !this.pressed;
+    this.count.push(1);
+  }
 }
